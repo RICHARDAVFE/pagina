3e469b568d939765a84aa5c58602304af88cd6b7
RICHARDAVFE <88010439+RICHARDAVFE@users.noreply.github.com>
2022-07-13 19:02:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MYSQL_DATABASE_HOST = 'localhost'
-    MYSQL_DATABASE_USER = 'root'
-    MYSQL_DATABASE_PASSWORD = ''
-    MYSQL_DATABASE_DB = 'flask_login'
+    MYSQL_DATABASE_HOST = 'us-cdbr-east-06.cleardb.net'
+    MYSQL_DATABASE_USER = 'b399f12758e8d9'
+    MYSQL_DATABASE_PASSWORD = '647f571f'
+    MYSQL_DATABASE_DB = 'heroku_69dc861d66128c1'
 
 
 config = {
